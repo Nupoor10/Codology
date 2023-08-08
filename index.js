14ab8a5160b9a362baa36971a605f912b5e43c58
Nupoor Shetye <shetyenupoor10@gmail.com>
2023-08-08 23:41:50 +0530

    Home Page Completed

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("start-btn").addEventListener("click", function() {
+    const languagesDiv = document.getElementById("languages");
+    languagesDiv.scrollIntoView({ behavior: "smooth" });
+  });
